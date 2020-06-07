7605b708f9d73a0587d81a680ce6c96d43c555b1
hanameee <hanameee@gmail.com>
2020-06-07 23:25:52 +0900

    [Config] Shadowed some components
    원본 저장소에서 src 폴더 복사해온 후 일부 세팅 수정.
    About 페이지와 Homepage 페이지 수정.
    구글 웹폰트로 글꼴 수정.

---
+++ src/components/hero.tsx
@@ -0,0 +1,31 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+const Hero = () => {
+    return (
+        <div
+            sx={{
+                boxSizing: `border-box`,
+                display: `flex`,
+                flexDirection: "column",
+                variant: `dividers.bottom`,
+                justifyContent: `center`,
+                alignItems: `center`,
+                my: [4, 5],
+                color: `secondary`,
+                borderBottom: `none`,
+            }}
+        >
+            <div sx={{ mb: [3, 4] }}>
+                개발자로서 배운 것들을 기록해나가는 공간.
+            </div>
+            <img
+                src={"/main.png"}
+                alt="Logo"
+                style={{ width: "50%" }}
+                sx={{ mb: [3, 4] }}
+            />
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/homepage.tsx
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { Link } from "gatsby";
+
+import Layout from "./layout";
+import Title from "./title";
+import Listing from "./listing";
+import List from "./list";
+import useMinimalBlogConfig from "../hooks/use-minimal-blog-config";
+import replaceSlashes from "../utils/replaceSlashes";
+// @ts-ignore
+import Hero from "./hero";
+// @ts-ignore
+import Bottom from "../texts/bottom";
+
+type PostsProps = {
+    posts: {
+        slug: string;
+        title: string;
+        date: string;
+        excerpt: string;
+        description: string;
+        timeToRead?: number;
+        tags?: {
+            name: string;
+            slug: string;
+        }[];
+    }[];
+};
+
+const Homepage = ({ posts }: PostsProps) => {
+    const { basePath, blogPath } = useMinimalBlogConfig();
+
+    return (
+        <Layout>
+            <Hero />
+            <Title text="최근 포스트 목록">
+                <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>
+                    모든 글 보기
+                </Link>
+            </Title>
+            <Listing posts={posts} showTags={false} />
+            <List>
+                <Bottom />
+            </List>
+        </Layout>
+    );
+};
+
+export default Homepage;
